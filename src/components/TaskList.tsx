d9e42b1943f76477e6699b69c5973419674a8dd4
Pashaie <pashaie@gmail.com>
2025-05-27 18:45:28 +0330

    components/TaskList:  TaskDetail - pre

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 
 //rfc - snippet 
+
+interface Task {
+    id: number;
+    name: string;
+    done: boolean;
+}
+
 export function TaskList() {
-    const [tasks, setTask] = useState([
+
+    const [tasks, setTask] = useState<Task[]>([
         { id: 1, name: "task 1", done: true },
         { id: 2, name: "task 2", done: true },
         { id: 3, name: "task 3", done: false },
         { id: 4, name: "task 4", done: false },
         { id: 5, name: "task 5", done: true },
     ]);
+
     return (
         <>
             <div>TaskList</div>
@@ -31,4 +40,16 @@ export function TaskList() {
         </>
 
     )
+}
+
+
+interface Props {
+    task: Task
+}
+function TaskDetail({ task }: Props) {
+    return <tr key={task.id}>
+        <td>{task.id}</td>
+        <td>{task.name}</td>
+        <td>{task.done ? "Yes" : "No"}</td>
+    </tr>
 }

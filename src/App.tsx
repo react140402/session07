44947ecc0ee313ca63b471837637ae5695b3b48b
Pashaie <pashaie@gmail.com>
2025-06-10 18:53:01 +0330

    AppLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import { useState } from 'react';
-import { PostList } from './components/Post/PostList'
-import { TaskList } from './components/Task/TaskList'
-import { TodoList } from './components/Todo/TodoList';
-import { SelectColor } from './components/SelectColor';
+
 import { AppProvider } from './AppContext';
+import AppLayout from './AppLayout';
 
 function App() {
-  const [showPostList, setShowPostList] = useState(true);
 
   return (
     <>
       <AppProvider>
-        <SelectColor />
-
-        <div className="container">
-          <TodoList />
-
-          <TaskList></TaskList>
-
-          <button onClick={() => setShowPostList(!showPostList)}>toggle</button>
-          {showPostList && <PostList></PostList>}
-        </div>
+        <AppLayout></AppLayout>
       </AppProvider>
     </>
   )
---
+++ src/AppLayout.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+
+import { PostList } from './components/Post/PostList'
+import { TaskList } from './components/Task/TaskList'
+import { TodoList } from './components/Todo/TodoList';
+import { SelectColor } from './components/SelectColor';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    getItem('Home', 'home', <PieChartOutlined />),
+    getItem('Task List', '1', <PieChartOutlined />),
+    getItem('Todos', '2', <DesktopOutlined />),
+];
+
+const App: React.FC = () => {
+    const [showPostList, setShowPostList] = useState(true);
+
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <div className="demo-logo-vertical" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }} />
+                <Content style={{ margin: '0 16px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }} items={[{ title: 'User' }, { title: 'Bill' }]} />
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                        }}
+                    >
+                        <SelectColor />
+
+                        <div className="container">
+                            <TodoList />
+
+                            <TaskList></TaskList>
+
+                            <button onClick={() => setShowPostList(!showPostList)}>toggle</button>
+                            {showPostList && <PostList></PostList>}
+                        </div>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default App;

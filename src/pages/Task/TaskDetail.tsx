f801f1c947b0780d08da7c2697f76f9f99d486cf
Pashaie <pashaie@gmail.com>
2025-07-01 19:46:38 +0330

    update task name

--- src/pages/Task/TaskDetail.tsx
+++ src/pages/Task/TaskDetail.tsx
@@ -1,7 +1,7 @@
-import { use } from "react";
+import { use, useState } from "react";
 import type { Task } from "./Task";
 import { AppContext } from "../../AppContext";
-import { deleteTask, toggleTask } from "./task.slice";
+import { deleteTask, toggleTask, updateTask } from "./task.slice";
 import { useAppDispatch } from "../../hooks";
 
 export interface Props {
@@ -10,9 +10,11 @@ export interface Props {
 export function TaskDetail({ task }: Props) {
     const { color } = use(AppContext);
     const dispatch = useAppDispatch();
+    const [mode, setMode] = useState<"read" | "edit">("read")
     return <tr>
         <td>{task.id}</td>
-        <td style={{ color }}>{task.name}</td>
+        <td style={{ color }}>{mode === "read" ? <span onDoubleClick={() => setMode("edit")}>{task.name}</span> :
+            <input onKeyUp={e => setMode(e.key === "Enter" || e.key === "Escape" ? "read" : "edit")} value={task.name} onChange={v => dispatch(updateTask({ ...task, name: v.target.value }))}></input>}</td>
         <td>{task.done ? "Yes" : "No"}
             <button onClick={() => dispatch(toggleTask(task))}>*️⃣</button>
             <button onClick={() => dispatch(deleteTask(task))}>❌</button>

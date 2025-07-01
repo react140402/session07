09749ef1b69b767a563b9c7d020e5f1a7255e51a
Pashaie <pashaie@gmail.com>
2025-07-01 21:14:30 +0330

    persist

--- src/pages/TaskZ/Task.store.ts
+++ src/pages/TaskZ/Task.store.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
+import { persist } from 'zustand/middleware';
 import type { Task } from './Task';
 
 interface State {
@@ -13,34 +14,39 @@ interface Action {
 }
 
 export const useTaskStore = create<State & Action>()(
-    immer((set) => ({
-        tasks: [
-            { id: 1, name: "task 1", done: true },
-            { id: 2, name: "task 2", done: true },
-            { id: 3, name: "task 3", done: false },
-            { id: 4, name: "task 4", done: false },
-            { id: 5, name: "task 5", done: true },
-        ],
+    persist(
+        immer((set) => ({
+            tasks: [
+                { id: 1, name: "task 1", done: true },
+                { id: 2, name: "task 2", done: true },
+                { id: 3, name: "task 3", done: false },
+                { id: 4, name: "task 4", done: false },
+                { id: 5, name: "task 5", done: true },
+            ],
 
-        createTask: (name: string, done: boolean) => set(state => {
-            const newTask = { id: Math.random(), name, done };
-            state.tasks.push(newTask);
-        }),
-        deleteTask: (task: Task) => set(state => {
-            state.tasks = state.tasks.filter(x => x.id !== task.id)
-        }),
-        toggleTask: (task: Task) => set(state => {
-            const task2 = state.tasks.find(x => x.id === task.id)
-            if (task2) {
-                task2.done = !task2.done;
-            }
-        }),
-        updateTask: (task: Task) => set(state => {
-            const task2 = state.tasks.find(x => x.id === task.id)
-            if (task2) {
-                task2.name = task.name,
-                    task2.done = task.done
-            }
-        }),
-    }))
+            createTask: (name: string, done: boolean) => set(state => {
+                const newTask = { id: Math.random(), name, done };
+                state.tasks.push(newTask);
+            }),
+            deleteTask: (task: Task) => set(state => {
+                state.tasks = state.tasks.filter(x => x.id !== task.id)
+            }),
+            toggleTask: (task: Task) => set(state => {
+                const task2 = state.tasks.find(x => x.id === task.id)
+                if (task2) {
+                    task2.done = !task2.done;
+                }
+            }),
+            updateTask: (task: Task) => set(state => {
+                const task2 = state.tasks.find(x => x.id === task.id)
+                if (task2) {
+                    task2.name = task.name,
+                        task2.done = task.done
+                }
+            }),
+        })),
+        {
+            name: 'task-list',
+        }
+    )
 );

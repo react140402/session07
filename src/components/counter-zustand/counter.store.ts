952762b917cde93da1d9bec2a9844246ab92062f
Pashaie <pashaie@gmail.com>
2025-07-01 20:50:53 +0330

    zustand + immer

--- src/components/counter-zustand/counter.store.ts
+++ src/components/counter-zustand/counter.store.ts
@@ -1,17 +1,27 @@
 import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
 
-interface Store {
+interface State {
     count: number;
+    name: string
+    color: string
+}
+interface Action {
     increment: () => void;
     decrement: () => void;
     reset: () => void;
     incrementByAmount: (amount: number) => void;
 }
 
-export const useCounterStore = create<Store>((set) => ({
-    count: 0,
-    increment: () => set(state => ({ count: state.count + 1 })),
-    decrement: () => set(state => ({ count: state.count - 1 })),
-    reset: () => set(state => ({ count: 0 })),
-    incrementByAmount: (amount: number) => set(state => ({ count: state.count + amount })),
-}));
+export const useCounterStore = create<State & Action>()(
+    immer((set) => ({
+        count: 0,
+        name: 'test',
+        color: 'red',
+
+        increment: () => set(state => { state.count++ }),
+        decrement: () => set(state => { state.count-- }),
+        reset: () => set(state => { state.count = 0 }),
+        incrementByAmount: (amount: number) => set(state => { state.count += amount }),
+    }))
+);

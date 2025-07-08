9ad1b19fa261f4d30d71a8dc36bfd314671de0ee
Pashaie <pashaie@gmail.com>
2025-07-08 20:31:47 +0330

    api error handling

--- src/AppApi.ts
+++ src/AppApi.ts
@@ -1,3 +1,20 @@
+import { notification } from "antd";
 import { Api } from "./Api";
+import axios from 'axios'
 
 export const appApi = new Api({ baseURL: "http://localhost:3010" });
+
+appApi.instance.interceptors.response.use(
+    (response) => {
+        return Promise.resolve(response)
+    },
+    (error) => {
+        console.log(error);
+        if (error.response?.data?.error) {
+            notification.error({ message: error.response?.data?.error })
+        } else {
+            notification.error({ message: "خطا" });
+        }
+        return Promise.reject(error);
+    }
+);

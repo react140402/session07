12193b5e3ce4557fa8a8105fc07a27b4d2b60765
Pashaie <pashaie@gmail.com>
2025-07-10 18:46:17 +0330

    Antd React 19 Compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import 'antd/'
+import '@ant-design/v5-patch-for-react-19';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
